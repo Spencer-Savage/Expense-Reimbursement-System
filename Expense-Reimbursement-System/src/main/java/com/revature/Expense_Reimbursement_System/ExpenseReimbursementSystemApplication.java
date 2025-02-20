53569725da2d2ca811c89404c38a10ac422a102f
Spencer Savage <email.spencersavage@gmail.com>
2025-02-19 18:11:49 -0600

    Initial SpringBoot setup

---
+++ Expense-Reimbursement-System/src/main/java/com/revature/Expense_Reimbursement_System/ExpenseReimbursementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Expense_Reimbursement_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReimbursementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReimbursementSystemApplication.class, args);
+	}
+
+}
---
+++ Expense-Reimbursement-System/src/test/java/com/revature/Expense_Reimbursement_System/ExpenseReimbursementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Expense_Reimbursement_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseReimbursementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
